fca30b364b1bcdf65db130d1f3aeb511b6a2adc1
Andrew Bubnov <jackblack.nt@ya.ru>
2020-06-16 11:22:18 +0500

    React has been added to project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
-class App {
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-  run = async (name = 'World') => {
-    console.log(`Hello ${name}`);
-    console.log([1, 2, [3, 4]].flat());
-  }
+const App = () => <h1>Hello World!</h1>
 
-}
-
-const app = new App();
-app.run()
-  .then(() => console.log('done'))
-  .catch(() => console.log('error'));
+ReactDOM.render(<App />, document.querySelector('.root'));
