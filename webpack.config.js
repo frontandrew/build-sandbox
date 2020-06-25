6420e8764cfe74d3d10653cb2d57c3cb4b46b2ab
Andrew Bubnov <jackblack.nt@ya.ru>
2020-06-25 09:53:10 +0500

    New loader configs have been added. Babel-loader has been installed.

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,31 @@ module.exports = {
 
   module: {
     rules: [
+
+      // Images
       {
-        test: /\.png$/,
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [
           {
-            loader: "file-loader"
+            loader: "file-loader",
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }
+        ]
+      },
+
+      // Fonts
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]'
+            }
           }
         ]
       }
