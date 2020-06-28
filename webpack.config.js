fde7f454ce942cec8f91bd59b29bad7aa7fa8c81
Andrew Bubnov <jackblack.nt@ya.ru>
2020-06-28 19:14:57 +0500

    Webpack has been cofigured for prod and dev build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,105 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: "development",
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env;
 
-  module: {
-    rules: [
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
 
-      // JS
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader' // упрощеный синтаксис блока 'use'
-      },
-
-      // CSS
-      {
-        test: /\.css$/,        
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-        ]
-      },
-
-      // SCSS or SASS
-      {
-        test: /\.s[ca]ss$/,
-        // если у лоадера нет доп опций, можно передавать только его имя
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ]
-      },
-
-      // Images
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]'
-            }
-          }
-        ]
-      },
-
-      // Fonts
-      {
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
-            }
-          }
-        ]
-      }
+  const getStyleLoaders = () => {
+    return [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'
     ]
-  },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Build SandBox',
-      buildTime: new Date().toString(),
-      template: 'index.html'
-    }),
+  }
 
-    new MiniCssExtractPlugin({
-      filename: '[name]-[hash:7].css'
-    }),
-  ]
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'Build SandBox',
+        buildTime: new Date().toString(),
+        template: 'index.html'
+      })
+    ];
+
+    if (isProd) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: '[name]-[hash:7].css'
+        })
+      )
+    }
+
+    return plugins
+  }
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    output: {
+      filename: isProd ? '[name]-[hash:7].js' : undefined
+    },
+
+    module: {
+      rules: [
+
+        // JS
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader' // упрощеный синтаксис блока 'use'
+        },
+
+        // CSS
+        {
+          test: /\.css$/,
+          use: getStyleLoaders()
+        },
+
+        // SCSS or SASS
+        {
+          test: /\.s[ca]ss$/,
+          // если у лоадера нет доп опций, можно передавать только его имя
+          use: [
+            ... getStyleLoaders(),
+            'sass-loader',
+          ]
+        },
+
+        // Images
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+
+        // Fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        }
+      ]
+    },
+
+    plugins: getPlugins(),
+
+    devServer: {
+      open: true,
+    }
+  }
 };
